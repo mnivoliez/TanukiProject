df5223430d342e5d7a8acb397a62c55c832685c2
Unknown <gomez.gwendo@gmail.com>
2017-11-24 15:32:35 +0100

    hat : move to the hand

---
+++ Assets/Scripts/PlayerHatAttack.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHatAttack : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject hat;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown("g"))
+        {
+            Transform hand = GameObject.FindGameObjectWithTag("Hand").transform;
+            hat.transform.parent = null;
+            hat.transform.parent = hand;
+            hat.transform.localPosition = new Vector3(-0.162f, 0.216f, -0.4f);
+            hat.transform.localRotation = Quaternion.Euler(new Vector3(-167.944f, 127.187f, -28.23599f));
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField]
+    private int playerHealthMax = 3;
+    [SerializeField]
+    private int playerHealthCurrent;
+    [SerializeField]
+    private Texture spriteHeart;
+
+    // Use this for initialization
+    void Start () {
+        playerHealthCurrent = playerHealthMax;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnGUI()
+    {
+        float x = 10;
+        for (int i = 1; i <= playerHealthCurrent; i++)
+        {
+            GUI.DrawTexture(new Rect(x, 10, 60, 60), spriteHeart, ScaleMode.ScaleToFit);
+            x += 70;
+        }
+    }
+}
