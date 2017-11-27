2f884ad6ac38de563167b33ddb0debe6bf485bc2
Jé OTH <jeremy.othenin@laposte.net>
2017-11-27 13:33:12 +0100

    Player attack maj

--- Assets/Scripts/PlayerHatAttack.cs
+++ Assets/Scripts/PlayerHatAttack.cs
@@ -6,10 +6,11 @@ public class PlayerHatAttack : MonoBehaviour {
 
     [SerializeField]
     private GameObject hat;
+    private Animator animBody;
 
-	// Use this for initialization
-	void Start () {
-        
+    // Use this for initialization
+    void Start () {
+        animBody = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -17,12 +18,16 @@ public class PlayerHatAttack : MonoBehaviour {
 
         if (Input.GetKeyDown("g"))
         {
+            
             Transform hand = GameObject.FindGameObjectWithTag("Hand").transform;
             hat.transform.parent = null;
             hat.transform.parent = hand;
             hat.transform.localPosition = new Vector3(-0.162f, 0.216f, -0.4f);
             hat.transform.localRotation = Quaternion.Euler(new Vector3(-167.944f, 127.187f, -28.23599f));
+            animBody.SetBool("isAttacking", true);
         }
+
+        //animBody.SetBool("isAttacking", false);
 
     }
 }
