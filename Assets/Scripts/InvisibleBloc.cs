023b0210dadf651255566c32361a72842179dc5e
Unknown <gomez.gwendo@gmail.com>
2017-12-11 15:50:12 +0100

    Some test for lantern (in scene test_gwen)

---
+++ Assets/Scripts/InvisibleBloc.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleBloc : MonoBehaviour {
+
+    private BoxCollider boxCollider;
+    private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start () {
+        boxCollider = transform.parent.GetComponent<BoxCollider>();
+        meshRenderer = transform.parent.GetComponent<MeshRenderer>();
+        boxCollider.enabled = false;
+        meshRenderer.enabled = false;
+    }
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Lantern")
+        {
+            boxCollider.enabled = true;
+            meshRenderer.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Lantern")
+        {
+            boxCollider.enabled = false;
+            meshRenderer.enabled = false;
+        }
+    }
+}
