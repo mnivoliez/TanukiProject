c11d3b360e39e298f1df095239b99c1d6b0ad071
Jé OTH <jeremy.othenin@laposte.net>
2017-12-13 19:31:01 +0100

    A Fucking godd Camera + Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public float rotateSpeed;
+    public Transform pivotCam;
+    public float maxViewAngle;
+    public float minViewAngle;
+    public bool useOffsetValues;
+    public bool inverseCam;
+
+    void Start () {
+
+        if (!useOffsetValues) {
+            offset = target.position - transform.position;
+        }
+
+        pivotCam.transform.position = target.position;
+        //pivotCam.transform.parent = target;
+        pivotCam.transform.parent = null;
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+
+	void LateUpdate () {
+        pivotCam.transform.position = target.position;
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivotCam.Rotate(0, horizontal, 0);
+
+        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        if (inverseCam) {
+            pivotCam.Rotate(vertical, 0, 0);
+        }
+        else {
+            pivotCam.Rotate(-vertical, 0, 0);
+        }
+
+        float desiredYangle = pivotCam.eulerAngles.y;
+        float desiredXangle = pivotCam.eulerAngles.x;
+
+        if (pivotCam.rotation.eulerAngles.x > maxViewAngle && pivotCam.rotation.eulerAngles.x < 180f) {
+            pivotCam.rotation = Quaternion.Euler(maxViewAngle, desiredYangle, 0);
+        }
+        if (pivotCam.rotation.eulerAngles.x > 180f && pivotCam.rotation.eulerAngles.x < 360f + minViewAngle) {
+            pivotCam.rotation = Quaternion.Euler(360f + minViewAngle, desiredYangle, 0);
+        }
+
+
+        Quaternion camRotation = Quaternion.Euler(desiredXangle, desiredYangle, 0);
+        transform.position = target.position - (camRotation * offset);
+
+        //transform.position = target.position - offset;
+        if(transform.position.y < target.position.y) {
+            transform.position = new Vector3(transform.position.x, target.transform.position.y, transform.position.z);
+        }
+
+        transform.LookAt(target.position);
+
+	}
+}
--- Assets/Scripts/Camera/MainCamera.cs
+++ Assets/Scripts/Camera/MainCamera.cs
@@ -52,13 +52,26 @@ public class MainCamera : MonoBehaviour {
 
 	void FixedUpdate(){
 		if (target != null) {
-			transform.position = Vector3.Lerp(transform.position, target.transform.position + CameraOffset, 40 * Time.deltaTime);
-			positionTarget = target.transform;
-			positionTarget.position.Set(target.transform.position.x, target.transform.position.y, target.transform.position.z);
-			transform.LookAt (positionTarget);
+            //transform.RotateAround(target.transform.position, new Vector3(0, 1, 0), Input.GetAxis("MoveCamera") * 2);
 
-		}
-	}
+            //CameraOffset.x += Input.GetAxis("MoveCamera");
+            //CameraOffset.z += Input.GetAxis("MoveCamera");
+
+            
+
+            //transform.RotateAround(target.transform.position, new Vector3(0, 1, 0), Input.GetAxis("MoveCamera") * 2);
+
+            transform.position = Vector3.Lerp(transform.position, target.transform.position + CameraOffset, 10 * Time.deltaTime);
+
+            transform.LookAt (target.transform.position);
+            //transform.position = Vector3.Lerp(transform.position, target.transform.position + CameraOffset, 40 * Time.deltaTime);
+            //transform.position = Vector3.Lerp(transform.position, target.transform.position + CameraOffset, 40 * Time.deltaTime);
+            //positionTarget = target.transform;
+            //positionTarget.position.Set(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+            //transform.LookAt(positionTarget);
+
+        }
+    }
 
 
 }
