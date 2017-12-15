2e3818b3eb9c789059d60dd8c8fc15c88ee43e62
Unknown <cam.lucas89@gmail.com>
2017-12-15 14:02:41 +0100

    ajout scène Planer/DoubleSaut/FeuFollet

---
+++ Assets/Scripts/PlateformeMobile.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlateformeMobile : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if (col.gameObject.CompareTag("Player")) {
+			col.transform.parent = transform;
+		}
+	}
+
+	void OnCollisionExit (Collision col)
+	{
+		if (col.gameObject.CompareTag("Player")) {
+			col.transform.parent = null;
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
