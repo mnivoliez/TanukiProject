f9af7638fca1670f32219491b081bc3ca574ca2c
Unknown <gomez.gwendo@gmail.com>
2018-01-31 17:21:04 +0100

    First state machine : mouvements

---
+++ Assets/Scripts/Player/Player States/FallState.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallState : State {
+
+    public FallState(CharacterController character) : base(character) {
+
+    }
+
+    public override void Tick() {
+
+        //IDLE
+        if (character.GetOnGround()) {
+
+            character.SetState(new IdleState(character));
+
+        }
+
+    }
+
+    public override void OnStateEnter() {
+        //Play anim
+        //Change for trigger
+        character.GetAnimbody().SetBool("isDoubleJumping", false);
+        character.GetAnimbody().SetBool("isJumping", false);
+    }
+
+    public override void OnStateExit() {
+
+    }
+
+}
---
+++ Assets/Scripts/Player/Player States/RunState.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RunState : State {
+
+    public RunState(CharacterController character) : base(character) {
+
+    }
+
+    public override void Tick() {
+
+        //IDLE
+        if (character.GetOnGround() && (Mathf.Abs(character.GetBody().velocity.x) + Mathf.Abs(character.GetBody().velocity.z)) < 0.1f) {
+            character.SetState(new RunState(character));
+        }
+
+
+
+        //JUMP
+        if (!character.GetOnGround() && character.GetBody().velocity.y > 0) {
+
+            character.SetState(new JumpState(character, false));
+
+        }
+
+
+        //FALL
+        if (!character.GetOnGround() && character.GetBody().velocity.y < 0) {
+
+            character.SetState(new FallState(character));
+
+        }
+
+    }
+
+    public override void OnStateEnter() {
+        //Play anim
+
+    }
+
+    public override void OnStateExit() {
+
+    }
+
+}
