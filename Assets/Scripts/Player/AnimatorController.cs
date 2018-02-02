175abbf1ede2a26e510f2a286160e5d276d499b0
Unknown <gomez.gwendo@gmail.com>
2018-02-02 16:04:45 +0100

    Refactor State machine movement ok

---
+++ Assets/Scripts/Player/AnimatorController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorController :  MonoBehaviour, /*IInterractState,*/ IMovementStateReceiver {
+
+    //Animation
+    private Animator animBody;
+
+    // Use this for initialization
+    void Start () {
+        animBody = GetComponent<Animator>();
+    }
+
+    public void UpdateState(MovementState state, float speed, float speedMultyplier) {
+
+        animBody.SetFloat("Speed", speed);
+        animBody.SetFloat("SpeedMultiplier", speedMultyplier);
+
+        //currentState = state;
+        //switch (state) {
+
+
+        //    case MovementState.Jump:
+                    
+                
+        //        break;
+
+        //    case MovementState.DoubleJump:
+                
+        //        break;
+        //}
+        
+    }
+
+    public void OnStateEnter(MovementState state) {
+        switch(state) {
+
+            case MovementState.Jump:
+                    animBody.SetBool("isJumping", true); //Change for trigger later
+                break;
+            case MovementState.DoubleJump:
+                    animBody.SetBool("isDoubleJumping", true); //Change for trigger later
+                break;
+        }
+    }
+
+    public void OnStateExit(MovementState state) {
+        switch (state) {
+            case MovementState.Jump:
+                    animBody.SetBool("isJumping", false); //Change for trigger later
+                break;
+
+            case MovementState.DoubleJump:
+                    animBody.SetBool("isDoubleJumping", false); //Change for trigger later
+                break;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Player/Player States/MovementStateController.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum MovementState {
+    Idle, Run, Jump, DoubleJump, Fall,
+}
+
+public struct MovementStateParam {
+    public Vector3 velocity;
+    public bool jumpRequired;
+    public bool grounded;
+}
+
+public class MovementStateController {
+
+    public MovementState GetNewState(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        switch (previous) {
+            case MovementState.Idle:
+                newState = ManageIdle(previous, param);
+                break;
+            case MovementState.Run:
+                newState = ManageRun(previous, param);
+                break;
+
+            case MovementState.Jump:
+                newState = ManageJump(previous, param);
+                break;
+
+
+            case MovementState.DoubleJump:
+                newState = ManageDoubleJump(previous, param);
+                break;
+
+
+            case MovementState.Fall:
+                newState = ManageFall(previous, param);
+                break;
+
+        }
+
+        return newState;
+    }
+
+    MovementState ManageJump(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        //IDLE
+        if (IsIdle(param)) {
+            newState = MovementState.Idle;
+        }
+
+        // STILL MOVING UP
+        if (IsGoingUp(param)) {
+            if (param.jumpRequired) {
+                newState = MovementState.DoubleJump;
+            }
+            else {
+                newState = MovementState.Jump;
+            }
+
+        }
+
+        //FAll
+        if (IsFalling(param)) {
+            newState = MovementState.Fall;
+        }
+        return newState;
+    }
+
+    MovementState ManageFall(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        //IDLE
+        if (IsIdle(param)) {
+            newState = MovementState.Idle;
+        }
+
+        if (IsRunning(param)) {
+            newState = MovementState.Run;
+        }
+        return newState;
+
+    }
+
+    MovementState ManageDoubleJump(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        //IDLE
+        if (IsIdle(param)) {
+            newState = MovementState.Idle;
+        }
+
+        //FAll
+        if (IsFalling(param)) {
+            newState = MovementState.Fall;
+        }
+
+        if (IsGoingUp(param)) {
+            newState = MovementState.DoubleJump;
+        }
+        return newState;
+    }
+
+    MovementState ManageRun(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        //IDLE
+        if (IsIdle(param)) {
+            newState = MovementState.Idle;
+        }
+
+        //JUMP
+        if (IsGoingUp(param)) {
+            newState = MovementState.Jump;
+        }
+
+        //FAll
+        if (IsFalling(param)) {
+            newState = MovementState.Fall;
+        }
+        return newState;
+    }
+
+    MovementState ManageIdle(MovementState previous, MovementStateParam param) {
+        MovementState newState = previous;
+        //RUN
+        if (IsRunning(param)) {
+            newState = MovementState.Run;
+        }
+
+        //JUMP
+        if (IsGoingUp(param)) {
+            newState = MovementState.Jump;
+        }
+
+        //FAll
+        if (IsFalling(param)) {
+            newState = MovementState.Fall;
+        }
+
+        return newState;
+    }
+
+    bool IsIdle(MovementStateParam param) {
+        return param.grounded && ((Mathf.Abs(param.velocity.x) + Mathf.Abs(param.velocity.z)) < 0.1f);
+    }
+
+    bool IsRunning(MovementStateParam param) {
+        return param.grounded && ((Mathf.Abs(param.velocity.x) + Mathf.Abs(param.velocity.z)) >= 0.1f);
+    }
+
+    bool IsGoingUp(MovementStateParam param) {
+        return !param.grounded && param.velocity.y > 0;
+    }
+
+    bool IsFalling(MovementStateParam param) {
+        return !param.grounded && param.velocity.y < 0;
+    }
+
+}
