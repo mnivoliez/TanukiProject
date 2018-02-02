13220390cb23a552e6fd10df2445950bdfb8ab2a
Jé OTH <jeremy.othenin@laposte.net>
2018-02-02 18:22:03 +0100

    Continue InteractState Machine

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -2,10 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-struct InputParams {
+struct InputMoveParams {
     public bool requestJump;
     public float moveX;
     public float moveZ;
+
+}
+
+struct InputInteractParams {
+    public bool GlideButton;
+    public bool MeleeAttackButton;
+    public bool DistantAttackButton;
+    public bool SpawnLureButton;
+    public bool TransformationButton;
+    public bool ResizeButton;
+    public bool ActivateButton;
+    public bool AbsorbButton;
+    public bool CarryButton;
+    public bool PushButton;
 
 }
 public class CharacterController : MonoBehaviour {
@@ -33,7 +47,10 @@ public class CharacterController : MonoBehaviour {
     private MovementState movementState;
     private MovementStateParam moveStateParameters;
     private MovementStateController moveStateCtrl;
-    InputParams inputParameters;
+    InputMoveParams inputMoveParameters;
+
+
+    InputInteractParams inputInteractParameters;
 
 
 
@@ -48,23 +65,26 @@ public class CharacterController : MonoBehaviour {
         animBody = GetComponent<AnimatorController>();
         moveStateParameters = new MovementStateParam();
         moveStateCtrl = new MovementStateController();
-        inputParameters = new InputParams();
+        inputMoveParameters = new InputMoveParams();
+        inputInteractParameters = new InputInteractParams();
     }
 
     private void Update() {
         
         onGround = IsGrounded();
-        UpdateInput();
+        UpdateMoveInput();
+        UpdateInteractInput();
         previousState = movementState;
 
         //deplacements
         ApplyInput();
 
         UpdateMoveStateParameters();
+        UpdateInteractStateParameters();
 
         movementState = moveStateCtrl.GetNewState(movementState, moveStateParameters);
 
-        float speed = Mathf.Sqrt(Mathf.Pow(inputParameters.moveX, 2) + Mathf.Pow(inputParameters.moveZ, 2));
+        float speed = Mathf.Sqrt(Mathf.Pow(inputMoveParameters.moveX, 2) + Mathf.Pow(inputMoveParameters.moveZ, 2));
 
         if (previousState != movementState) {
             animBody.OnStateExit(previousState);
@@ -116,13 +136,30 @@ public class CharacterController : MonoBehaviour {
     }
 
 
-    InputParams UpdateInput() {
+    InputMoveParams UpdateMoveInput() {
 
-        inputParameters.requestJump = Input.GetButtonDown("Jump");
-        inputParameters.moveX = Input.GetAxis("Horizontal");
-        inputParameters.moveZ = Input.GetAxis("Vertical");
+        inputMoveParameters.requestJump = Input.GetButtonDown("Jump");
+        inputMoveParameters.moveX = Input.GetAxis("Horizontal");
+        inputMoveParameters.moveZ = Input.GetAxis("Vertical");
 
-        return inputParameters;
+        return inputMoveParameters;
+
+    }
+
+    InputInteractParams UpdateInteractInput() {
+
+        inputInteractParameters.GlideButton = Input.GetButton("Jump");
+        inputInteractParameters.MeleeAttackButton = Input.GetButton("Fire1");
+        inputInteractParameters.DistantAttackButton = Input.GetButton("Fire2");
+        inputInteractParameters.SpawnLureButton = Input.GetButton("Lure");
+        inputInteractParameters.TransformationButton = Input.GetButton("Transformation");
+        inputInteractParameters.ResizeButton = Input.GetButton("Resize");
+        inputInteractParameters.ActivateButton = Input.GetButton("Fire3");
+        inputInteractParameters.AbsorbButton = Input.GetButton("Fire3");
+        inputInteractParameters.CarryButton = Input.GetButton("Fire3");
+        //inputInteractParameters.PushButton = Input.GetButton("Fire3");
+
+        return inputInteractParameters;
 
     }
 
@@ -131,27 +168,27 @@ public class CharacterController : MonoBehaviour {
         bool canJump = !(movementState == MovementState.DoubleJump || movementState == MovementState.Fall)/* ou si maudit et pas en state jump / fall */;
         Debug.Log("Here the state:" + movementState);
         //JUMP
-        if (inputParameters.requestJump && canJump) {
+        if (inputMoveParameters.requestJump && canJump) {
             body.velocity = new Vector3(0, jumpForce, 0);
         }
         
-        inputVelocityAxis = new Vector3(inputParameters.moveX, body.velocity.y, inputParameters.moveZ);
+        inputVelocityAxis = new Vector3(inputMoveParameters.moveX, body.velocity.y, inputMoveParameters.moveZ);
         //Orientation du personnage
-        orientationMove = (transform.forward * inputParameters.moveZ) + (transform.right * inputParameters.moveX);
+        orientationMove = (transform.forward * inputMoveParameters.moveZ) + (transform.right * inputMoveParameters.moveX);
         inputVelocityAxis = inputVelocityAxis.normalized * moveSpeed;
         inputVelocityAxis.y = body.velocity.y;
 
 
         //AIR CONTROL
         if (!onGround) {
-            body.velocity = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * new Vector3((inputParameters.moveX * airControl), inputVelocityAxis.y, (inputParameters.moveZ * (airControl * 2)));
+            body.velocity = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * new Vector3((inputMoveParameters.moveX * airControl), inputVelocityAxis.y, (inputMoveParameters.moveZ * (airControl * 2)));
         }
         else {
             body.velocity = Quaternion.AngleAxis(Camera.main.transform.eulerAngles.y, Vector3.up) * inputVelocityAxis;
         }
 
         //Move player on direction based on camera
-        if (inputParameters.moveX != 0 || inputParameters.moveZ != 0) {
+        if (inputMoveParameters.moveX != 0 || inputMoveParameters.moveZ != 0) {
             transform.rotation = Quaternion.Euler(0, pivot.rotation.eulerAngles.y, 0);
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(orientationMove.x, 0f, orientationMove.z));
             playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
@@ -160,9 +197,14 @@ public class CharacterController : MonoBehaviour {
 
     void UpdateMoveStateParameters() {
         moveStateParameters.velocity = body.velocity;
-        moveStateParameters.jumpRequired = inputParameters.requestJump;
+        moveStateParameters.jumpRequired = inputMoveParameters.requestJump;
         moveStateParameters.grounded = IsGrounded();
     }
+
+    void UpdateInteractStateParameters() {
+        //A TERMINER !
+    }
+
 
     public bool GetOnGround() {return onGround;}
     public float GetJumpForce() { return jumpForce; }
