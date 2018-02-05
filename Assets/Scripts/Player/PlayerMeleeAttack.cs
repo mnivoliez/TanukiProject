9cf1997b85b9c1663fa005f17be03ab9bb5d7139
Hanna <Hanna>
2018-02-05 14:09:33 +0100

    clean animator controller and mask animation

--- Assets/Scripts/Player/PlayerMeleeAttack.cs
+++ Assets/Scripts/Player/PlayerMeleeAttack.cs
@@ -73,9 +73,7 @@ public class PlayerMeleeAttack : MonoBehaviour {
 			timerAttack = 0;
 		}
 
-		if ((this.animBody.GetCurrentAnimatorStateInfo(0).IsName("JumpInsantPostAttack") || 
-			this.animBody.GetCurrentAnimatorStateInfo(0).IsName("RunInsantPostAttack") || 
-			this.animBody.GetCurrentAnimatorStateInfo(0).IsName("PostAttack")) && leafInHand) {
+		if ((this.animBody.GetCurrentAnimatorStateInfo(0).IsName("JumpInsantPostAttack") || this.animBody.GetCurrentAnimatorStateInfo(0).IsName("PostAttack")) && leafInHand) {
 			attackRange.SetActive(false);
 			leafHead.SetActive(true);
 			leafHand.SetActive(false);
