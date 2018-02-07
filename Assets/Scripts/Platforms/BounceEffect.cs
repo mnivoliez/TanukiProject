f19474639a7dd3ef8ca701e44f303740d930c21a
Yami_nK <gaetan.bayon-de-noyer@cpe.fr>
2018-02-07 15:07:08 +0100

    Bounce Effect Revisited

--- Assets/Scripts/Platforms/BounceEffect.cs
+++ Assets/Scripts/Platforms/BounceEffect.cs
@@ -4,27 +4,11 @@ using UnityEngine;
 
 public class BounceEffect : MonoBehaviour {
 
-   
-    public int forceRebond = 30;
 
-    // Use this for initialization
-    void Start() {
+    public float coeffRebond;
 
-    }
-
-    // Update is called once per frame
-    void Update() {
-
-    }
-
-    private void OnCollisionEnter(Collision collision) {
-
+    private void OnCollisionExit(Collision collision) {
         Rigidbody entity = collision.gameObject.GetComponent<Rigidbody>();
-        entity.velocity = Vector3.zero;
-        if (collision.gameObject.name == "Player") {
-            //collision.gameObject.GetComponent<PlayerController>().SetIsJumping(true);
-            Debug.Log("BOING");
-        }
-        entity.AddForce(Vector3.up * forceRebond, ForceMode.VelocityChange);
+        entity.velocity += Vector3.up * coeffRebond;
     }
 }
