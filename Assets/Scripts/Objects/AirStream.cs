3c52d2783d90f3fff5aa4a6b60d61d76dc50bc1f
Jé OTH <jeremy.othenin@laposte.net>
2018-02-07 17:12:21 +0100

    Airstream scene

---
+++ Assets/Scripts/Objects/AirStream.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirStream : MonoBehaviour {
+
+
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other) {
+
+        if (other.gameObject.CompareTag("Player")) {
+            Rigidbody bodyObject = other.gameObject.GetComponent<Rigidbody>();
+            
+        }
+        
+    }
+
+
+
+}
