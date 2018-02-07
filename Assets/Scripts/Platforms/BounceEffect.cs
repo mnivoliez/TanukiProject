8837aff9d8cc658874f8e48393bfa134e7c9b7c2
Yami_nK <28897448+Luraminaki@users.noreply.github.com>
2018-02-07 16:31:18 +0100

    Boncing Fixed.

--- Assets/Scripts/Platforms/BounceEffect.cs
+++ Assets/Scripts/Platforms/BounceEffect.cs
@@ -7,8 +7,19 @@ public class BounceEffect : MonoBehaviour {
 
     public float coeffRebond;
 
-    private void OnCollisionExit(Collision collision) {
-        Rigidbody entity = collision.gameObject.GetComponent<Rigidbody>();
-        entity.velocity += Vector3.up * coeffRebond;
+
+    private void OnCollisionEnter(Collision collision) {
+        Rigidbody Koda = collision.gameObject.GetComponent<Rigidbody>();
+        Vector3 Koda_old_velocity = Koda.velocity;
+        ContactPoint contact = collision.contacts[0];
+
+        Koda.AddForce(-collision.contacts[0].normal * 200, ForceMode.Impulse); // 200 = coeffRebond
+
+        Debug.Log("BOING");
     }
+
+    /*private void OnCollisionExit(Collision collision) {
+        Rigidbody Koda = collision.gameObject.GetComponent<Rigidbody>();
+        Koda.velocity += Vector3.up * coeffRebond;
+    }*/
 }
