7c289a5e8db7cc061ecf45b3d2fd24b8204d32cc
MAI <maixuanhien.vn93@gmail.com>
2018-02-09 15:37:32 +0100

    add ShadowDirect prefab, scripts ShadowDirect for character

--- Assets/Scripts/Player/CharacterController.cs
+++ Assets/Scripts/Player/CharacterController.cs
@@ -140,7 +140,6 @@ public class CharacterController : MonoBehaviour {
         }
 
         animBody.UpdateState(movementState, speed, moveSpeed);
-
     }
 
     void OnCollisionEnter(Collision coll) {
