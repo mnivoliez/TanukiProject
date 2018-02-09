6f9aff338054c1a52d0f9e8575520887427420b0
MAI <maixuanhien.vn93@gmail.com>
2018-02-09 15:37:32 +0100

    add ShadowDirect prefab, scripts ShadowDirect for character

---
+++ Assets/Scripts/Player/ShadowDirect/ShadowDirectController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowDirectController : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject shadowDirect;
+    [SerializeField]
+    private float rayCastDistance = 30.0f;
+
+    private GameObject clone;
+    private Vector3 position;
+    private float maxSize;
+    private float minSize;
+
+    RaycastHit hit;
+
+    private void Start() {
+        maxSize = 1.0f;
+        minSize = 0.1f;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, rayCastDistance)) {
+            position = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
+            clone = Instantiate(shadowDirect, position, transform.rotation);
+        }
+    }
+
+    private void Update() {
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit, rayCastDistance)) {
+            position = new Vector3(hit.point.x, hit.point.y + 0.5f, hit.point.z);
+            clone.transform.position = position;
+
+            float distance = Vector3.Distance(transform.position, hit.point);
+            float size = maxSize * distance / rayCastDistance;
+            size = 1f - Mathf.Clamp(size, minSize, maxSize);
+            Vector3 scale = new Vector3(maxSize, maxSize, maxSize) * size;
+            clone.transform.localScale = scale;
+            //Debug.Log(size);
+        }
+    }
+}
