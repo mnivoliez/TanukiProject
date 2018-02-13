cec9abb8af87307111f5d5a2275b0ed24f193355
mnivoliez <mathieu.nivoliez@laposte.net>
2018-02-13 14:34:41 +0100

    I have never been so annoyed by a bug INSIDE THE EDITOR!!!!!!

--- Assets/LanternsRenderingController.cs
+++ Assets/LanternsRenderingController.cs
@@ -10,11 +10,15 @@ public class LanternsRenderingController : MonoBehaviour
     private List<float> _distances;
     private bool _isDirty;
 
+    // If changed, restart unity to it to be effective.
+    private const int MAX_NUMBER_OF_LANTERNS = 5;
     void Awake()
     {
         _lanterns = GameObject.FindGameObjectsWithTag("Lantern");
         _positions = new List<Vector4>(5);
         _distances = new List<float>(5);
+        Shader.SetGlobalVectorArray("_centers", new Vector4[MAX_NUMBER_OF_LANTERNS]);
+        Shader.SetGlobalFloatArray("_distances", new float[MAX_NUMBER_OF_LANTERNS]);
         foreach (var lantern in _lanterns)
         {
             _positions.Add(lantern.transform.position);
