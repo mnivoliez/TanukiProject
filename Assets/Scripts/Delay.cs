a7c3be8689c34296a01115600e376e692da72d8d
Unknown <cam.lucas89@gmail.com>
2018-02-14 12:18:30 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Delay.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delay : MonoBehaviour {
+    public GameObject prefabBumperDroite;
+    public GameObject prefabBumperGauche;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("SpawnBumperDroite", 0f, 15f);
+        InvokeRepeating("SpawnBumperGauche", 0f, 15f);
+    }
+
+	void SpawnBumperDroite() {
+        Destroy(Instantiate(prefabBumperDroite, transform.position, Quaternion.identity, transform), 32);
+        
+    }
+
+    void SpawnBumperGauche()
+    {
+        Destroy(Instantiate(prefabBumperGauche, transform.position, Quaternion.identity, transform), 30);
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Objects/Hide_Show_Block.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hide_Show_Block : MonoBehaviour {
+	//pour que le script marche il faux que les objet qui apparaisse est leur mesh et collider (sur le père) déactiver
+	// et pour les objet qui disparaisse il faux que le mesh et le collider du père soit activer
+	private Collider boxCollider;
+	private MeshRenderer meshRenderer;
+
+	// Use this for initialization
+	void Start() {
+		boxCollider = transform.parent.GetComponent<Collider>();
+		meshRenderer = transform.parent.GetComponent<MeshRenderer>();
+		//boxCollider.enabled = false;
+		//meshRenderer.enabled = false;
+	}
+
+	void OnTriggerEnter(Collider collider) {
+		if (collider.gameObject.tag == "Lantern") {
+			meshRenderer.enabled = !boxCollider.enabled;
+			boxCollider.enabled = !boxCollider.enabled;
+
+			print (meshRenderer.enabled.ToString());
+		}
+	}
+
+	void OnTriggerExit(Collider collider) {
+		if (collider.gameObject.tag == "Lantern") {
+			meshRenderer.enabled = !boxCollider.enabled;
+			boxCollider.enabled = !boxCollider.enabled;
+		
+			print (meshRenderer.enabled.ToString());
+		}
+	}
+
+}
