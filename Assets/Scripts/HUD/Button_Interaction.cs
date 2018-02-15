77ff8b98762d42ad435330d09e5d9a16c7c82508
Yami_nK <28897448+Luraminaki@users.noreply.github.com>
2018-02-15 14:31:36 +0100

    Corrected Menu Animations. Selected Keyboard still buggy (Work in Progress)

---
+++ Assets/Scripts/HUD/Button_Interaction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Button_Interaction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnPointerEnter(Animator animator) {
+        animator.SetTrigger("Highlighted");
+    }
+
+    public void OnPointerExit(Animator animator) {
+        animator.SetTrigger("Normal");
+    }
+}
