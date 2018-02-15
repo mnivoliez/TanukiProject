b9a195e27f4e69e82006c7d182b340c96829020b
Unknown <gomez.gwendo@gmail.com>
2018-02-15 12:21:21 +0100

    Fix about attack on yokai

---
+++ Assets/Scripts/Enemies/Minor Yokais/DetectRangeYokaiGeneral.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectRangeYokaiGeneral : MonoBehaviour {
+
+    private YokaiGeneralBehavior parentBehavior;
+    private Collider areaCollider;
+
+	// Use this for initialization
+	void Start () {
+        parentBehavior = GetComponentInParent<YokaiGeneralBehavior>();
+        areaCollider = GetComponent<Collider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (parentBehavior.GetIsKnocked() && areaCollider.enabled == true) {
+            areaCollider.enabled = false;
+        }
+	}
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            parentBehavior.SetTarget(other.gameObject);
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.tag == "Player") {
+            parentBehavior.SetTarget(null);
+        }
+    }
+}
