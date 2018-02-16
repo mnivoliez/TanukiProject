c252eef2562d1f11e09bb288b623d3b4f5586fd2
mnivoliez <mathieu.nivoliez@laposte.net>
2018-02-16 10:17:34 +0100

    Tak

---
+++ Assets/LerpMatTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LerpMatTest : MonoBehaviour
+{
+
+    [SerializeField] private Material _lightMat;
+    [SerializeField] private Material _darkMat;
+    public float duration = 2.0F;
+    private Renderer _rend;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rend = GetComponent<Renderer>();
+        _rend.material = _darkMat;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float lerp = Mathf.PingPong(Time.time, duration) / duration;
+        _rend.material.Lerp(_darkMat, _lightMat, lerp);
+    }
+}
