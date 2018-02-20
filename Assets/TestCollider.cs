2406c823572224d40786ef6e89e6cc5c467b7fe1
Unknown <gomez.gwendo@gmail.com>
2018-02-20 16:04:14 +0100

    Lure changes : rigidbody with kinematic on detectArea on yokai

---
+++ Assets/TestCollider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider) {
+        Debug.Log(collider.gameObject.tag);
+    }
+}
