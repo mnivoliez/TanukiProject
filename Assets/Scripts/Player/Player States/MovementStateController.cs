8ce4f2b1dec6a30ad563a8b9541fa4b33a0605d5
Hanna <Hanna>
2018-02-20 15:02:03 +0100

    fix jump async + airstream + minor bug fixes

--- Assets/Scripts/Player/Player States/MovementStateController.cs
+++ Assets/Scripts/Player/Player States/MovementStateController.cs
@@ -130,21 +130,27 @@ public class MovementStateController {
 		//DebugLogLocal ("Fall manage");
         //IDLE
 		if (IsIdle(param)) {
-			DebugLogLocal ("fall idle");
+			DebugLogLocal ("pushup idle");
             newState = MovementState.Idle;
         }
 
 		//RUN
 		if (IsRunning(param)) {
-			DebugLogLocal ("fall run");
+			DebugLogLocal ("pushup run");
             newState = MovementState.Run;
-        }
+		}
 
-        //FALL
-        if (IsFalling(param)) {
-            DebugLogLocal ("fall run");
-            newState = MovementState.Fall;
-        }
+		//FALL
+		if (IsFalling(param)) {
+			DebugLogLocal ("pushup fall");
+			newState = MovementState.Fall;
+		}
+
+		//JUMP
+		/*if (IsGoingUp(param) && !param.inAirStream) {
+			DebugLogLocal ("pushup jump");
+			newState = MovementState.Jump;
+		}*/
 
         return newState;
 
