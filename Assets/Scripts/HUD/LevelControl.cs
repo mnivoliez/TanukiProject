523b6acfba2aabc0723e86da45196936a1d7ac41
Yami_nK <28897448+Luraminaki@users.noreply.github.com>
2018-02-26 11:45:15 +0100

    Added FadIN and FadeOUT for Transitions. Added Arrow selection for HUD. Added LevelControl Script to trigger FADES effect.

---
+++ Assets/Scripts/HUD/LevelControl.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelControl : MonoBehaviour {
+
+    public int index;
+    public string levelName;
+
+    public Image Black;
+    public Animator anim;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.CompareTag("Player")) {
+            StartCoroutine(Fading());
+        }
+    }
+
+    IEnumerator Fading() {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => Black.color.a == 1);
+        SceneManager.LoadScene(index);
+    }
+}
