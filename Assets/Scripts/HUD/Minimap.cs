b0c9ae4ccc07dec74f16cb124ad5413357963290
Yami_nK <28897448+Luraminaki@users.noreply.github.com>
2018-03-05 13:03:07 +0100

    Almost fixed Minimap. Figuring out last bugs.

--- Assets/Scripts/HUD/Minimap.cs
+++ Assets/Scripts/HUD/Minimap.cs
@@ -6,14 +6,19 @@ public class Minimap : MonoBehaviour {
 
     //public Transform player;
     private Canvas CanvasMinimap;
+    private GameObject player;
 
     void Start() {
         CanvasMinimap = GetComponent<Canvas>();
-}
+        player = GameObject.FindGameObjectWithTag("Player");
+        Debug.Log(player.name);
+    }
 
     private void LateUpdate() {
         //Vector3 newPosition = PlayerPrefab.position;
-        GameObject player = GameObject.Find("Player");
+        //GameObject player = GameObject.Find("Player");
+        //player = GameObject.FindGameObjectsWithTag("Player");
+
         Vector3 newPosition = player.transform.position;
         newPosition.y = transform.position.y;
         transform.position = newPosition;
