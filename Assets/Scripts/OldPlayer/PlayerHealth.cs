09b4929a568533263c8d6045856c284a71b0f501
Jé OTH <jeremy.othenin@laposte.net>
2018-03-05 14:30:45 +0100

    Add Health system + prefab healObject

--- Assets/Scripts/OldPlayer/PlayerHealth.cs
+++ Assets/Scripts/OldPlayer/PlayerHealth.cs
@@ -4,32 +4,42 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour {
 
-    [SerializeField]
-    private float playerHealthMax = 3.0f;
-    [SerializeField]
-    private float playerHealthCurrent;
-    [SerializeField]
-    private Texture spriteHeart;
+    [SerializeField] [Range(3, 7)] private float playerHealthMax = 3.0f;
+    [SerializeField] [Range(0, 7)] private float playerHealthCurrent = 3.0f;
 
-    // Use this for initialization
+
     void Start() {
-        playerHealthCurrent = playerHealthMax;
+
     }
 
-    // Update is called once per frame
     void Update() {
 
     }
 
-    void OnGUI() {
-        float x = 10;
-        for (int i = 1; i <= playerHealthCurrent; i++) {
-            GUI.DrawTexture(new Rect(x, 10, 60, 60), spriteHeart, ScaleMode.ScaleToFit);
-            x += 70;
-        }
-    }
+    //void OnGUI() {
+    //    float x = 10;
+    //    for (int i = 1; i <= playerHealthCurrent; i++) {
+    //        GUI.DrawTexture(new Rect(x, 10, 60, 60), spriteHeart, ScaleMode.ScaleToFit);
+    //        x += 70;
+    //    }
+    //}
 
     public void LooseHP(float dame) {
         playerHealthCurrent = playerHealthCurrent - dame;
+    }
+
+    public void GainHP(float nbHP) {
+        playerHealthCurrent = playerHealthCurrent + nbHP;
+        if(playerHealthCurrent> playerHealthMax) {
+            playerHealthCurrent = playerHealthMax;
+        }
+    }
+
+    public float GetHealthCurrent() {
+        return playerHealthCurrent;
+    }
+
+    public float GetHealthMax() {
+        return playerHealthMax;
     }
 }
