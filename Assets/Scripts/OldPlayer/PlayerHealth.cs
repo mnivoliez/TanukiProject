2d1329ca5590d437c09be577d4437334aad7a7b1
Jé OTH <jeremy.othenin@laposte.net>
2018-03-06 11:10:54 +0100

    Update HealthSystem + Yokai make & Koda take damage

--- Assets/Scripts/OldPlayer/PlayerHealth.cs
+++ Assets/Scripts/OldPlayer/PlayerHealth.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour {
 
     [SerializeField] [Range(3, 7)] private float playerHealthMax = 3.0f;
     [SerializeField] [Range(0, 7)] private float playerHealthCurrent = 3.0f;
 
+    [SerializeField] private GameObject respawnPoint;
+    [SerializeField] private GameObject deathTransition;
+    private Image Black;
+    private Animator anim;
+    private Animator animPlayer;
 
     void Start() {
+        animPlayer = gameObject.GetComponent<Animator>();
+        GameObject transitionImageInstance = Instantiate(deathTransition);
 
+        Black = transitionImageInstance.GetComponent<Image>();
+        anim = transitionImageInstance.GetComponent<Animator>();
     }
 
     void Update() {
@@ -24,13 +34,16 @@ public class PlayerHealth : MonoBehaviour {
     //    }
     //}
 
-    public void LooseHP(float dame) {
-        playerHealthCurrent = playerHealthCurrent - dame;
+    public void LooseHP(float dmg) {
+        playerHealthCurrent = playerHealthCurrent - dmg;
+        if (playerHealthCurrent <= 0) {
+            PlayerDie();
+        }
     }
 
     public void GainHP(float nbHP) {
         playerHealthCurrent = playerHealthCurrent + nbHP;
-        if(playerHealthCurrent> playerHealthMax) {
+        if (playerHealthCurrent > playerHealthMax) {
             playerHealthCurrent = playerHealthMax;
         }
     }
@@ -41,5 +54,21 @@ public class PlayerHealth : MonoBehaviour {
 
     public float GetHealthMax() {
         return playerHealthMax;
+    }
+
+    public void PlayerDie() {
+        animPlayer.SetBool("isDead", true);
+        StartCoroutine(Fading());
+        playerHealthCurrent = playerHealthMax;
+    }
+
+    IEnumerator Fading() {
+        Debug.Log("COUCOU !");
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => Black.color.a == 1);
+        Debug.Log("never !");
+        anim.SetBool("Fade", false);
+        animPlayer.SetBool("isDead", false);
+        animPlayer.transform.SetPositionAndRotation(respawnPoint.transform.position, Quaternion.identity);
     }
 }
