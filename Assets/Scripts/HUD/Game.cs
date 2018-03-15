648747778551b360b0960fc8b9616713a081d3cc
Yami_nK <gaetan.bayon-de-noyer@cpe.fr>
2018-03-15 15:53:24 +0100

    Fused Matt && Gaetan Save_Load. Missing Get-er from Koda &co.

--- Assets/Scripts/HUD/Game.cs
+++ Assets/Scripts/HUD/Game.cs
@@ -1,6 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+using UnityEngine.SceneManagement;
 
 [System.Serializable] // Tells Unity that this script can be serialized—in other words, that we can save all the variables in this script. MUST BE IN ALL SCRIPTS THAT NEEDS TO BE SAVED !
 public class Game {
@@ -8,10 +12,77 @@ public class Game {
     public static Game current;
     //public Zone_Level = Zone; // Need to know the current zone the player is. We need a Zone script that handles the state of the zone, the checkpoints, etc ...
     //public Character Koda; // Need to know the state Koda is in : His unlocked powers, HP remaining, current checkpoint unlocked, the amount of Yokai killed, ... Script required !
+    private int slot;
+    public var scene_path;
+
+    void Awake()
+    {
+        scene_path = Application.persistentDataPath + "/savedGames_slot_" + slot + ".gs";
+
+        if (current == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            this.scene_save_path = scene_path;
+            current = this;
+        }
+        else if (current != this)
+        {
+            current.scene_save_path = scene_path;
+            Destroy(gameObject);
+        }
+    }
 
     public Game() {
         //Zone = new Zone_Level();
-        //Koda = new Character();
+        Koda = new Player();
+        Level_Info = new LevelData();
     }
 
+}
+
+public class Player{
+
+    GameObject koda = GameObject.FindGameObjectWithTag("Player");
+    //PlayerHealth koda_health = koda.GetComponent<PlayerHealth>();
+
+    PlayerData playerData = new PlayerData();
+
+    playerData.hp = koda.GetHealthCurrent();
+    playerData.hp_max = koda.GetHealthMax();
+    playerData.caught_Yokai = koda.GetCaughtYokai();
+    
+    playerData.check_point = koda.GetRespawnPointPosition();
+    playerData.current_scene = SceneManager.GetActiveScene().name;
+
+    playerData.power_jump = koda.GetPowerJump();
+    playerData.power_ball = koda.GetPowerBall();
+    playerData.power_shrink = koda.GetPowerShrink();
+}
+
+[Serializable]
+class SaveData
+{
+    public PlayerData _player;
+    public Dictionary<String, LevelData> _levels;
+}
+
+[Serializable]
+class PlayerData
+{
+    public float hp;
+    public float hp_max;
+    public int caught_yokai;
+
+    public string current_scene;
+    public Transform check_point;
+
+    public bool power_jump;
+    public bool power_ball;
+    public bool power_shrink;
+}
+
+[Serializable]
+class LevelData
+{
+    public List<Transform> _enemiesPos;
 }
--- Assets/Scripts/HUD/Save_Load.cs
+++ Assets/Scripts/HUD/Save_Load.cs
@@ -7,19 +7,20 @@ using System.IO;
 public class Save_Load {
 
     public static List<Game> savedGames = new List<Game>(); // Create a list of type "Game" and call it "savedGames" (Needto create a public class Game that will handle the state of the overall game)
+    public int selected_slot;
 
     public static void Save() {
         savedGames.Add(Game.current); // Adds our current game to our list of saved games.
         BinaryFormatter bf = new BinaryFormatter(); // Willhandle the serialization work.
-        FileStream file = File.Create(Application.persistentDataPath + "/savedGames.gd"); // Pathwway to a new file that we can send data to.
+        FileStream file = File.Create(savedGames.scene_path); // Pathwway to a new file that we can send data to.
         bf.Serialize(file, Save_Load.savedGames);
         file.Close();
     }
 
     public static void Load() {
-        if (File.Exists(Application.persistentDataPath + "/savedGames.gd")) {
+        if (File.Exists(Application.persistentDataPath + "/savedGames_slot_" + selected_slot + ".gs")) {
             BinaryFormatter bf = new BinaryFormatter();
-            FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
+            FileStream file = File.Open(Application.persistentDataPath + "/savedGames_slot_" + selected_slot + ".gs");
             Save_Load.savedGames = (List<Game>)bf.Deserialize(file); // Check https://unity3d.com/fr/learn/tutorials/topics/scripting/persistence-saving-and-loading-data video at 44:40
             file.Close();
         }
