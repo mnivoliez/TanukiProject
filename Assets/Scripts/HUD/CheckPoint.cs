3a81b976fdef7a7fbdf0f33bd3f23c3531a3d2d7
Yami_nK <gaetan.bayon-de-noyer@cpe.fr>
2018-03-16 16:02:12 +0100

    Save on Checkpoint Working.

--- Assets/Scripts/HUD/CheckPoint.cs
+++ Assets/Scripts/HUD/CheckPoint.cs
@@ -23,31 +23,33 @@ public class CheckPoint : MonoBehaviour {
 
     private void OnTriggerEnter(Collider other) {
 
-        float hp_max = koda_health.GetHealthMax();
-        float hp_current = koda_health.GetHealthCurrent();
+        if(other.name == "Player") { 
+            float hp_max = koda_health.GetHealthMax();
+            float hp_current = koda_health.GetHealthCurrent();
 
-        //playerData.caught_Yokai = koda_power.GetCaughtYokai();
+            //playerData.caught_Yokai = koda_power.GetCaughtYokai();
 
-        Transform check_point = koda_power.GetRespawnPointPosition();
-        float check_point_x = check_point.position.x;
-        float check_point_y = check_point.position.y;
-        float check_point_z = check_point.position.z;
+            Transform check_point = koda_power.GetRespawnPointPosition();
+            float check_point_x = check_point.position.x;
+            float check_point_y = check_point.position.y;
+            float check_point_z = check_point.position.z;
 
-        string current_scene = SceneManager.GetActiveScene().name;
+            string current_scene = SceneManager.GetActiveScene().name;
 
-        bool power_jump = koda_power.GetPowerJump();
-        bool power_ball = koda_power.GetPowerBall();
-        bool power_shrink = koda_power.GetPowerShrink();
+            bool power_jump = koda_power.GetPowerJump();
+            bool power_ball = koda_power.GetPowerBall();
+            bool power_shrink = koda_power.GetPowerShrink();
 
-        Game this_game = new Game();
-        this_game.selected_slot = 0;
-        this_game.PreSave_Game(hp_max, hp_current, check_point_x, check_point_y, check_point_z, current_scene, power_jump, power_ball, power_shrink);
+            Game this_game = new Game();
+            this_game.selected_slot = 0;
+            this_game.PreSave_Game(hp_max, hp_current, check_point_x, check_point_y, check_point_z, current_scene, power_jump, power_ball, power_shrink);
 
-        Save_Load.savedGames = this_game;
-        Save_Load.selected_slot = this_game.selected_slot;
+            Save_Load.savedGames = this_game;
+            Save_Load.selected_slot = this_game.selected_slot;
 
-        Save_Load.Save();
+            Save_Load.Save();
 
-        //Debug.Log(Application.persistentDataPath);
+            //Debug.Log(Application.persistentDataPath);
+        }
     }
 }
