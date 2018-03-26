3ef3e9d4a2e81775344ad47e21b3fc43dc42f385
Unknown <gomez.gwendo@gmail.com>
2018-03-26 16:55:46 +0200

    Liana ok with 4 assets randomized

--- Assets/Editor/LianaGenerator.cs
+++ Assets/Editor/LianaGenerator.cs
@@ -10,12 +10,18 @@ public class LianaGenerator : EditorWindow {
     private static List<GameObject> lianaObjects;
     private float lianaLenght;
     private string lenght;
-    private static GameObject lianaMesh;
+    private static List<GameObject> lianaMeshs;
+    private static GameObject lianaMesh1;
+    private static GameObject lianaMesh2;
+    private static GameObject lianaMesh3;
+    private static GameObject lianaMesh4;
+    static System.Random rnd = new System.Random();
     private bool autoLenght;
 
     [MenuItem("Generate/All Liana")]
     public static void GenerateAllLiana() {
         lianaObjects = new List<GameObject>();
+        lianaMeshs = new List<GameObject>();
 
         GameObject[] objectsFinded = GameObject.FindGameObjectsWithTag("Liana");
         foreach (GameObject objectFinded in objectsFinded) {
@@ -28,7 +34,8 @@ public class LianaGenerator : EditorWindow {
     [MenuItem("Generate/Selected Liana")]
     public static void GenerateSelectedLiana() {
         lianaObjects = new List<GameObject>();
-
+        lianaMeshs = new List<GameObject>();
+        
         GameObject[] selectedObjects = Selection.gameObjects;
         foreach (GameObject selectedObject in selectedObjects) {
             if (selectedObject.tag == "Liana") {
@@ -45,7 +52,24 @@ public class LianaGenerator : EditorWindow {
     }
 
     private void Generate() {
-        Vector3 sizeLianaMesh = lianaMesh.GetComponent<MeshRenderer>().bounds.size;
+
+        if (lianaMesh1 != null) {
+            lianaMeshs.Add(lianaMesh1);
+        }
+
+        if (lianaMesh2 != null) {
+            lianaMeshs.Add(lianaMesh2);
+        }
+
+        if (lianaMesh3 != null) {
+            lianaMeshs.Add(lianaMesh3);
+        }
+
+        if (lianaMesh4 != null) {
+            lianaMeshs.Add(lianaMesh4);
+        }
+        
+        Vector3 sizeLianaMesh = lianaMesh1.GetComponent<MeshRenderer>().bounds.size;
 
         float.TryParse(lenght, out lianaLenght);
 
@@ -53,7 +77,7 @@ public class LianaGenerator : EditorWindow {
             lianaLenght = 10;
         }
 
-        float partSizeLianaMesh = sizeLianaMesh.y * 0.2f;
+        float partSizeLianaMesh = sizeLianaMesh.y * 0.15f;
         int nbLianaPart = (int)(lianaLenght / (sizeLianaMesh.y - partSizeLianaMesh));
 
         foreach (GameObject liana in lianaObjects) {
@@ -65,11 +89,27 @@ public class LianaGenerator : EditorWindow {
                 nbLianaPart = AutoSize(liana, sizeLianaMesh.y);
             }
 
+            int r = 0;
+
             float positionYLianaPart = 0;
             for (int i = 0; i < nbLianaPart; i++) {
-                GameObject lianaPart = Instantiate(lianaMesh, liana.transform.position, lianaMesh.transform.rotation);
+                int rTemp = r;
+                if (lianaMeshs.Count > 1) {
+                    while (rTemp == r) {
+                        rTemp = rnd.Next(lianaMeshs.Count);
+                    }
+                } else {
+                    rTemp = rnd.Next(lianaMeshs.Count);
+                }
+                
+                r = rTemp;
+
+                int rotationY = rnd.Next(360);
+
+                GameObject lianaPart = Instantiate(lianaMeshs[r], liana.transform.position, lianaMesh1.transform.rotation);
                 lianaPart.transform.parent = liana.transform;
                 lianaPart.transform.localPosition = new Vector3(0, positionYLianaPart, 0);
+                lianaPart.transform.Rotate(0, rotationY, 0);
                 lianaPart.GetComponent<SpringJoint>().connectedBody = bodyToConnect;
                 bodyToConnect = lianaPart.GetComponent<Rigidbody>();
                 positionYLianaPart -= sizeLianaMesh.y - partSizeLianaMesh;
@@ -79,7 +119,11 @@ public class LianaGenerator : EditorWindow {
 
     void OnGUI() {
 
-        lianaMesh = EditorGUILayout.ObjectField("Find liana mesh", lianaMesh, typeof(GameObject)) as GameObject;
+        lianaMesh1 = EditorGUILayout.ObjectField("Find liana prefab", lianaMesh1, typeof(GameObject)) as GameObject;
+        lianaMesh2 = EditorGUILayout.ObjectField("Find liana prefab", lianaMesh2, typeof(GameObject)) as GameObject;
+        lianaMesh3 = EditorGUILayout.ObjectField("Find liana prefab", lianaMesh3, typeof(GameObject)) as GameObject;
+        lianaMesh4 = EditorGUILayout.ObjectField("Find liana prefab", lianaMesh4, typeof(GameObject)) as GameObject;
+
         autoLenght = EditorGUILayout.Toggle("Auto lenght liana", autoLenght);
         EditorGUI.BeginDisabledGroup(autoLenght);
         lenght = EditorGUILayout.TextField("Lenght liana", lenght);
@@ -89,7 +133,6 @@ public class LianaGenerator : EditorWindow {
         if (GUILayout.Button("OK")) {
             ResetLiana();
             Generate();
-            Close();
         }
     }
 
@@ -97,7 +140,7 @@ public class LianaGenerator : EditorWindow {
         int nbLianaPart = 0;
         RaycastHit hit;
         float lenght = 0;
-        float partSizeLianaMesh = sizeLianaMesh * 0.2f;
+        float partSizeLianaMesh = sizeLianaMesh * 0.15f;
 
         if (Physics.Raycast(liana.transform.position, Vector3.down, out hit, 30)) {
             lenght = hit.distance - 0.5f;
