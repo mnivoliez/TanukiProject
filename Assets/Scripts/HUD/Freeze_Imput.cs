db07630fb02febca043fb89d0fb67bc13f369ce1
Jérémy <jeremy.othenin@laposte.net>
2018-04-05 10:07:39 +0200

    Timeline boss 1 + freeze input

---
+++ Assets/Scripts/HUD/Freeze_Imput.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Freeze_Imput : MonoBehaviour {
+
+    [SerializeField] private bool isfreeze;
+
+	void Start () {
+        GameObject.FindGameObjectWithTag("Player").GetComponent<InputController>().SetFreezeInput(isfreeze);
+	}
+	
+
+	void Update () {
+		
+	}
+}
