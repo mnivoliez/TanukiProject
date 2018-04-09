560ffddf86486f1838db3b6527aec9ea14c26176
mnivoliez <mathieu.nivoliez@laposte.net>
2018-04-09 16:50:24 +0200

    When activating the stand, the lantern did not return to spawn. Several elements were in cause: * The stand has to be in Catchable layer * The stand has to be tag as LoveHotel * The interact cone was not well define * The catchable object ware not prioritized on activable ones. We defined a real cone starting before koda for interaction, we define catchable as prioritized over activable and we update prefab for tag and layer. Also the nearest detect script has been moved to PlayerV2.

--- Assets/Scripts/Objects/LanternStandController.cs
+++ Assets/Scripts/Objects/LanternStandController.cs
@@ -4,21 +4,11 @@ using UnityEngine;
 
 public class LanternStandController : MonoBehaviour
 {
-
-    private Vector3 origin_pos;
-    private Quaternion origin_rot;
     [SerializeField]
     private LanternController lantern;
 
-    void Awake()
-    {
-        origin_pos = lantern.transform.position;
-        origin_rot = lantern.transform.rotation;
-    }
-
     public void RecallLantern()
     {
-        lantern.transform.position = origin_pos;
-        lantern.transform.rotation = origin_rot;
+        lantern.Respawn();
     }
 }
