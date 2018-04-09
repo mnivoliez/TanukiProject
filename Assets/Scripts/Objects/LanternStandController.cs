8880eac734cdb89dba631735d4078e5261059cb4
Jérémy <jeremy.othenin@laposte.net>
2018-04-09 14:01:05 +0200

    Infinite time on light water

--- Assets/Scripts/Objects/LanternStandController.cs
+++ Assets/Scripts/Objects/LanternStandController.cs
@@ -2,22 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LanternStandController : MonoBehaviour
-{
+public class LanternStandController : MonoBehaviour {
 
     private Vector3 origin_pos;
     private Quaternion origin_rot;
     [SerializeField]
     private LanternController lantern;
 
-    void Awake()
-    {
+    void Awake() {
         origin_pos = lantern.transform.position;
         origin_rot = lantern.transform.rotation;
     }
 
-    public void RecallLantern()
-    {
+    public void RecallLantern() {
         lantern.transform.position = origin_pos;
         lantern.transform.rotation = origin_rot;
     }
