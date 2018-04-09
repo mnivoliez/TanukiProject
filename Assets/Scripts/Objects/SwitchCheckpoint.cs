cd0c9a67b43e3af94fd752ec831e0aecb91e2cd4
Arsia <cam.lucas89@gmail.com>
2018-04-09 18:52:30 +0200

    Update Z2-P1

---
+++ Assets/Scripts/Objects/SwitchCheckpoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchCheckpoint : MonoBehaviour
+{
+    [SerializeField] private SwitchObject[] actionOnSwitch;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+
+            foreach (SwitchObject obj in actionOnSwitch)
+            {
+                if (obj.gameObject != null)
+                {
+                    obj.gameObject.SetActive(obj.action.Equals(ActionLantern.Activate));
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
