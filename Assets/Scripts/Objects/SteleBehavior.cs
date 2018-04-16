5ba57939307333e80a3cbc841ec5c14727b2d6d3
Unknown <jeremy.othenin@laposte.net>
2018-04-16 13:41:40 +0200

    Add Hitodama reach stele

---
+++ Assets/Scripts/Objects/SteleBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SteleBehavior : MonoBehaviour {
+
+    [SerializeField] private GameObject nextStele;
+
+    private void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.CompareTag("Player")) {
+            HitodamaController hitodama = GameObject.FindGameObjectWithTag("Hitodama").GetComponent<HitodamaController>();
+            hitodama.SetIsGuiding(true);
+            hitodama.SetTargetStele(nextStele);
+        }
+
+    }
+}
