654f10a7a0f742640891212e9bf86d2ac43cc5af
Unknown <jeremy.othenin@laposte.net>
2018-04-17 11:45:12 +0200

    Change switch behavior + stele behavior + remove timer absorption

--- Assets/Scripts/Objects/SwitchCheckpoint.cs
+++ Assets/Scripts/Objects/SwitchCheckpoint.cs
@@ -2,34 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchCheckpoint : MonoBehaviour
-{
+public class SwitchCheckpoint : MonoBehaviour {
     [SerializeField] private SwitchObject[] actionOnSwitch;
 
-    // Use this for initialization
-    void Start()
-    {
-    }
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.CompareTag("Player")) {
 
-
-    void OnTriggerEnter(Collider collider)
-    {
-        if (collider.gameObject.CompareTag("Player"))
-        {
-
-            foreach (SwitchObject obj in actionOnSwitch)
-            {
-                if (obj.gameObject != null)
-                {
+            foreach (SwitchObject obj in actionOnSwitch) {
+                if (obj.gameObject != null) {
                     obj.gameObject.SetActive(obj.action.Equals(ActionLantern.Activate));
                 }
             }
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
 }
