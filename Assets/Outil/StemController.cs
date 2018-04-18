636642f2d281aeccc601c576cea0947b0c816d75
Unknown <gomez.gwendo@gmail.com>
2018-04-18 14:30:44 +0200

    WIP stem
    position ok

--- Assets/Outil/StemController.cs
+++ Assets/Outil/StemController.cs
@@ -7,10 +7,7 @@ public class StemController : MonoBehaviour {
 	
 	private PathCreator creator;
 	private PathPlatform path;
-	private float percentBetweenWaypoints = 0;
-	private int fromWaypointIndex = 0;
 	// Travel time between two waypoints in second
-	private float waypointPeriod = 7f;
 	[SerializeField] private GameObject prefabStem1;
 	[SerializeField] private GameObject prefabStem2;
 	[SerializeField] private GameObject prefabStem3;
@@ -19,34 +16,56 @@ public class StemController : MonoBehaviour {
 	void Start() {
 		creator = GetComponent<PathCreator>();
 		path = creator.path;
-		fromWaypointIndex = 0;
 		GenerateStem();
 	}
 
 	void GenerateStem() {
 
-		int i = 0;
-		while (i <= 7) {
-			Debug.Log ("i: " + i);
-			percentBetweenWaypoints = i * (1f/waypointPeriod);
+        GameObject previousStem = null;
+        float waypointPeriod = 7f;
+        float percentBetweenWaypoints = 0;
+        int nbSegment = path.NumSegments;
+        Vector3 positionStem = new Vector3();
+        GameObject stem = null;
+        float sizeYStem = prefabStem1.GetComponent<MeshRenderer>().bounds.size.y;
 
-			if (percentBetweenWaypoints >= 1 && fromWaypointIndex < path.NumPoints) {
-				fromWaypointIndex++;
-				Debug.Log ("reset i ! because percentBetweenWaypoints: " + percentBetweenWaypoints);
-				i = 0;
-				Debug.Log ("i: " + i);
-				Debug.Log ("segment: " + fromWaypointIndex);
-				percentBetweenWaypoints--;
-			}
+        for (int i = 0; i < nbSegment; i++) {
+            float lenghtArcBezier = GetLengthArcBezier(i);
+            int nbPartStem = (int)(lenghtArcBezier / sizeYStem);
+            waypointPeriod = nbPartStem;
+            for (int j = 0; j <= nbPartStem; j++) {
+                if (j < nbPartStem) {
+                    percentBetweenWaypoints = j * (1f / waypointPeriod);
+                    stem = Instantiate(prefabStem1);
+                    positionStem = GetCurvesVector(i, percentBetweenWaypoints);
+                    stem.transform.position = positionStem;
+                    stem.name = "Segment" + i + "_Part" + j;
+                } else {
+                    positionStem = previousStem.transform.position;
+                }
 
-			Instantiate (prefabStem1);
-			prefabStem1.transform.position = GetCurvesVector (fromWaypointIndex, percentBetweenWaypoints);
-			prefabStem1.name = "Segment" + fromWaypointIndex + "_Part" + i;
-			if (percentBetweenWaypoints < 1 || fromWaypointIndex >= path.NumPoints) {
-				i++;
-			}
-		}
+                if (previousStem != null) {
+                    Vector3 vecDir = (stem.transform.position - previousStem.transform.position);
+                    Vector3 vecDirDegree = (180.0f / Mathf.PI) * vecDir;
+                    //previousStem.transform.LookAt(vecDir + previousStem.transform.up);
+                    Debug.Log(vecDirDegree);
+                    //previousStem.transform.rotation = Quaternion.FromToRotation(previousStem.transform.position, vecDir);
+                    previousStem.transform.rotation = Quaternion.Euler(vecDirDegree);
+                }
+                if (j < nbPartStem - 1) {
+                    previousStem = stem;
+                }
+            }
+        }
 	}
+
+    float GetLengthArcBezier(int index) {
+        Vector3[] points = path.GetPointsInSegment(index);
+        float chord = (points[3] - points[0]).magnitude;
+        float cont_net = (points[0] - points[1]).magnitude + (points[2] - points[1]).magnitude + (points[3] - points[2]).magnitude;
+        float lenghtArcBezier = (cont_net + chord) / 2;
+        return lenghtArcBezier;
+    }
 
 	Vector3 GetCurvesVector (int index, float keyValue) {
 		Vector3[] points = path.GetPointsInSegment(index);
