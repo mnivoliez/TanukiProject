26a14bb8148aefeb39b1031a0d7a938d76249eb4
Yami_nK <gaetan.bayon-de-noyer@cpe.fr>
2018-04-18 17:05:32 +0200

    Many changes for the upcoming "SaveManager" (It's not anactual script, just the idea of the process)

--- Assets/Scripts/Save&Load&Scenes&Collectable/CollectableManager.cs
+++ Assets/Scripts/Save&Load&Scenes&Collectable/CollectableManager.cs
@@ -15,59 +15,57 @@ public class CollectableManager : MonoBehaviour {
 
     void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         Game.Reset_Game(); // Will reset ONLY if the current scene is "Zone Tuto"
+        FindYokaiGeneralAndEnable();
     }
 
-    void FirstVisit(string currentScene) {
+    void isPurified(string currentScene) {
         switch (currentScene) {
             case "Z1-P1-complete":
-                Game.playerData.visited_Z1P1 = true;
+                if (Game.playerData.lightBoss1) {
+                    //LIGHT
+                }
                 break;
 
             case "Z1-P2-complete":
-                Game.playerData.visited_Z1P2 = true;
+                if (Game.playerData.lightBoss1) {
+                    //LIGHT
+                }
                 break;
 
             case "Z1-P3-complete":
-                Game.playerData.visited_Z1P3 = true;
+                if (Game.playerData.lightBoss1) {
+                    //LIGHT
+                }
                 break;
 
             case "Z2-P1-complete":
-                Game.playerData.visited_Z2P1 = true;
+                if (Game.playerData.lightBoss2) {
+                    //LIGHT
+                }
                 break;
 
             case "Z2-P2-complete":
-                Game.playerData.visited_Z2P2 = true;
+                if (Game.playerData.lightBoss2) {
+                    //LIGHT
+                }
                 break;
 
             case "Z2-P3-complete":
-                Game.playerData.visited_Z2P3 = true;
-                break;
-
-            case "Scene_AirStream":
-                Game.playerData.visited_test = true;
+                if (Game.playerData.lightBoss2) {
+                    //LIGHT
+                }
                 break;
         }
     }
 
-    void FindYokaiGeneralAndDisableIfPossible(int yokaiCaught) {
+    void FindYokaiGeneralAndDisable() {
         yokaiGeneralCurrentScene = 0;
         gOList = (GameObject[])FindObjectsOfType(typeof(GameObject));
 
         for (int i = 0; i < gOList.Length; i++) {
             if (gOList[i].name.Contains("Yokai_General")) {
-                yokaiGeneralCurrentScene = yokaiGeneralCurrentScene + 1;
+                Debug.Log("FoundYa !");
             }
-        }
-
-        if(yokaiCaught >= yokaiGeneralCurrentScene) {
-            for (int i = 0; i < gOList.Length; i++) {
-                if (gOList[i].name.Contains("Yokai_General") && gOList[i].GetComponent<YokaiAIv2Controller>().canBeKilled) {
-                    gOList[i].SetActive(false);
-                }
-            }
-        }
-        else {
-            int yokaiToRemove = yokaiGeneralCurrentScene - yokaiCaught;
         }
     }
 }
