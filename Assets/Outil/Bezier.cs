77fd54c0baa56bec7a5cfe18c8b5054165b1994d
Emmanuel <voxelse@gmail.com>
2018-04-20 17:19:29 +0200

    Improve Platform Tool
    - Add Rotation Support
    - Add Passenger Support
    - Change default Path size
    - Optimize Bezier computation

--- Assets/Outil/Bezier.cs
+++ Assets/Outil/Bezier.cs
@@ -2,15 +2,38 @@
 
 public class Bezier {
 
-	public static Vector3 EvaluateQuadratic(Vector3 a, Vector3 b, Vector3 c, float t) {
-		Vector3 p0 = Vector3.Lerp(a, b, t);
-		Vector3 p1 = Vector3.Lerp(b, c, t);
-		return Vector3.Lerp(p0,p1,t);
+	public static Vector3 Evaluate (Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * p0 +
+			2f * oneMinusT * t * p1 +
+			t * t * p2;
 	}
 
-	public static Vector3 EvaluateQubic(Vector3 a, Vector3 b, Vector3 c, Vector3 d, float t) {
-		Vector3 p0 = EvaluateQuadratic(a, b, c, t);
-		Vector3 p1 = EvaluateQuadratic(b, c, d, t);
-		return Vector3.Lerp(p0,p1,t);
+	public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+		return
+			2f * (1f - t) * (p1 - p0) +
+			2f * t * (p2 - p1);
 	}
+
+	public static Vector3 Evaluate (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			oneMinusT * oneMinusT * oneMinusT * p0 +
+			3f * oneMinusT * oneMinusT * t * p1 +
+			3f * oneMinusT * t * t * p2 +
+			t * t * t * p3;
+	}
+	
+	public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+		t = Mathf.Clamp01(t);
+		float oneMinusT = 1f - t;
+		return
+			3f * oneMinusT * oneMinusT * (p1 - p0) +
+			6f * oneMinusT * t * (p2 - p1) +
+			3f * t * t * (p3 - p2);
+	}
+
 }
