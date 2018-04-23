4a57a4ae3a57360a5beb765dfffd01479daa550d
Shad0war <jeremy.othenin@laposte.net>
2018-04-23 18:57:15 +0200

    Make possible background story on stele

--- Assets/Scripts/Objects/SteleBehavior.cs
+++ Assets/Scripts/Objects/SteleBehavior.cs
@@ -13,16 +13,51 @@ public class SteleBehavior : MonoBehaviour {
     [SerializeField] private float pathPointsSpacing = 1f;
     [SerializeField] private float pathResolution = 1f;
 
+    bool isActive;
+    int nbTuto = 0;
+    int currentTuto = 0;
+
     void Start() {
-        if(nextStele) {
+        nbTuto = transform.childCount;
+        currentTuto = 0;
+
+        if (nextStele) {
             creator = GetComponent<PathCreator>();
 		    path = creator.path;
         }
 	}
 
+    private void Update() {
+        if (isActive) {
+            if (Input.GetButtonDown("Jump")) {
+                transform.GetChild(currentTuto).gameObject.SetActive(false);
+                //================================================
+                SoundController.instance.SelectHUD("TutoPictureExit");
+                //================================================
+                if (currentTuto < nbTuto - 1) {
+                    currentTuto++;
+                    transform.GetChild(currentTuto).gameObject.SetActive(true);
+                }
+                else {
+                    GameObject.FindGameObjectWithTag("Player").GetComponent<InputController>().SetFreezeInput(false);
+                    Destroy(gameObject);
+                }
+
+            }
+        }
+    }
+
     private void OnTriggerEnter(Collider other) {
 
         if (other.gameObject.CompareTag("Player")) {
+
+            //================================================
+            SoundController.instance.SelectHUD("PauseOpenClose");
+            //================================================
+            isActive = true;
+            transform.GetChild(0).gameObject.SetActive(true);
+            other.gameObject.GetComponent<InputController>().SetFreezeInput(true);
+
             HitodamaController hitodama = GameObject.FindGameObjectWithTag("Hitodama").GetComponent<HitodamaController>();
             if (lastStele) {
                 hitodama.SetIsGuiding(false);
