5f68b6836c38e0c4a7663a0cc2bc97eeaef9ce61
Emmanuel <voxelse@gmail.com>
2018-04-24 17:20:20 +0200

    Add Run and Landing FX

---
+++ Assets/PawProjector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PawProjector : MonoBehaviour {
+	
+	private Material mat;
+	private float progress = 1f;
+
+	[SerializeField]
+	private float speedProgress = 0.01f;
+
+	void Start() {
+		mat = GetComponent<Projector>().material = new Material(GetComponent<Projector>().material);
+	}
+
+	void Update () {
+		mat.SetFloat("_PawProgress", progress);
+		progress -= speedProgress;
+
+		if(progress < 0)
+			Destroy(transform.parent.gameObject);
+	}
+}
