9bd9b11fa3e7bf3ad843f3485428f94dc6b26245
Yami_nK <28897448+Luraminaki@users.noreply.github.com>
2018-04-27 11:18:33 +0200

    New SD for intro. Testing in process.

--- Assets/Scripts/HUD/SoundController.cs
+++ Assets/Scripts/HUD/SoundController.cs
@@ -45,6 +45,7 @@ public class SoundController : MonoBehaviour {
     [SerializeField] private AudioClip HUDPauseAccept;
     [SerializeField] private AudioClip HUDTutoPictureClose;
     [SerializeField] private AudioClip HUDCredits;
+    [SerializeField] private AudioClip HUDIntro;
 
     [Header("KODA")]
     [Space(10)]
@@ -178,6 +179,11 @@ public class SoundController : MonoBehaviour {
         if (SceneManager.GetActiveScene().name == "Credit-Scene") {
             KillAll();
             PlayHUDTheme(HUDCredits, true);
+        }
+
+        if (SceneManager.GetActiveScene().name == "Introduction") {
+            KillAll();
+            PlayHUDTheme(HUDIntro, true);
         }
 
         if (SceneManager.GetActiveScene().name == "Boss1" && !Game.playerData.lightBoss1 && !Game.playerData.Boss1KO) {
