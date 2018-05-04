92976789c3587ce9d77afc51724039a52e07f298
Shad0war <jeremy.othenin@laposte.net>
2018-05-04 16:33:08 +0200

    THE PURIFICATOR

---
+++ Assets/Scripts/Save&Load&Scenes&Collectable/PurificatorBehavior.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PurificatorBehavior : MonoBehaviour {
+
+    [SerializeField] private SwitchObject[] actionOnSwitch;
+    [SerializeField] private bool isZ1;
+    [SerializeField] private bool isZ2;
+
+    void Start () {
+        if ((isZ1 && Game.playerData.lightBoss1) || (isZ2 && Game.playerData.lightBoss2)) {
+            foreach (SwitchObject obj in actionOnSwitch) {
+                if (obj.gameObject != null) {
+                    obj.gameObject.SetActive(obj.action.Equals(ActionLantern.Activate));
+                }
+            }
+        }
+
+    }
+	
+}
